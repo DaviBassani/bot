ecc95fe8708041f974f74ee70d369e50b0e0433b
Davi Bassani <bassani.davi@gmail.com>
2023-09-18 22:14:24 -0300

    Upload with errors on wikipedia commands

--- main.py
+++ main.py
@@ -4,17 +4,36 @@ pip install discord.py
 import discord
 from discord.ext import commands
 import secret
+import time
+import wikipedia
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 token = secret.TOKEN()
+wikipedia.set_lang("pt-br")
 
-print("""
+@bot.event
+async def on_ready():
+     print("""
+Status: Online
 Author: Davi Bassani
 Pertence a: Dev Server BR
 Nome: Dev Server Bot
 """)
-      
+           
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Olá, {ctx.author.name}!")
+
+@bot.command()
+async def horas(ctx):
+    await ctx.send(f"Agora são {time.strftime('%H:%M')}")
+
+@bot.command()
+async def wiki(ctx, arg):
+    busca = wikipedia.search(arg)
+    await ctx.send(f"{str(busca)}")
+
 bot.run(token)
